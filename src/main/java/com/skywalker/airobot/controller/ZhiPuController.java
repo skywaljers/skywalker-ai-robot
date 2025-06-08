270e4cbc510657bb3aabacdb5ec79f437cd6830d
skywaljers <2648760447@qq.com>
2025-06-08 10:00:00 +0800

    3.3节：接入智谱AI

---
+++ src/main/java/com/skywalker/airobot/controller/ZhiPuController.java
@@ -0,0 +1,50 @@
+package com.skywalker.airobot.controller;
+
+import jakarta.annotation.Resource;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.zhipuai.ZhiPuAiChatModel;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+/**
+ * @author baijj
+ * @date 8/6/2025
+ */
+@RestController
+@RequestMapping("/v4/ai")
+public class ZhiPuController {
+
+    @Resource
+    private ZhiPuAiChatModel chatModel;
+
+    /**
+     * 普通对话
+     * @param message
+     * @return
+     */
+    @GetMapping("/generate")
+    public String generate(@RequestParam(value = "message", defaultValue = "你是谁？") String message) {
+        // 一次性返回结果
+        return chatModel.call(message);
+    }
+
+    /**
+     * 流式对话
+     * @param message
+     * @return
+     */
+    @GetMapping(value = "/generateStream", produces = "text/html;charset=utf-8")
+    public Flux<String> generateStream(@RequestParam(value = "message", defaultValue = "你是谁？") String message) {
+        // 构建提示词
+        Prompt prompt = new Prompt(new UserMessage(message));
+
+        // 流式输出
+        return chatModel.stream(prompt)
+                .mapNotNull(chatResponse -> chatResponse.getResult().getOutput().getText());
+
+    }
+}
